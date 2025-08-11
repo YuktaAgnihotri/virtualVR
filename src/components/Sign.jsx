9bb70f307c8b00ad13daffcae4ef4652b7c117ff
YuktaAgnihotri <yuktaagnihotri8585@gmail.com>
2025-08-12 03:37:08 +0530

    signn

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Sign() {
   return (
-    <div className=' relative bg-amber-400 w-[40vw] h-[30vh] m-auto top-50 z-10 ' >Sign</div>
+    <div className=' relative bg-neutral-600 w-[40vw] h-[30vh] m-auto top-20 z-10 ' >Sign</div>
   )
 }
 
