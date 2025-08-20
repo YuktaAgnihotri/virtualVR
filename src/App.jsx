c4e53295675e4e41300a344a340994b79ce7fe25
Yukta Agnihotri <yuktaagnihotri8585@gmail.com>
2025-08-20 21:36:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ function App() {
      
         <Route path='/' element={<MainLayout/>} />
       
-       <Route  path="/Sign" element={<Sign/>}> />
-      <Route  path="/CreateAccount" element={<CreateAccount/>}> />
+       <Route  path="/sign" element={<Sign/>}> />
+      <Route  path="/createAccount" element={<CreateAccount/>}> />
     </Routes>
      </div>
     </AuthContext>
