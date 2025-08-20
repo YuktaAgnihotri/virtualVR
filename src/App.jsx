0d7494945fe977c153f25cbc451ec855265df3e2
Yukta Agnihotri <yuktaagnihotri8585@gmail.com>
2025-08-20 22:11:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import FeaturesSection from './components/FeaturesSection'
 import Pricing from './components/Pricing'
