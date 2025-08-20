0b5f5e55f87358b4c4c93f9609c6351fe83f57bf
Yukta Agnihotri <yuktaagnihotri8585@gmail.com>
2025-08-20 22:25:20 +0530

    Update Sign.jsx

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
 import UserPg from './UserPg';
-import { useContext } from 'react';
+//import { useContext } from 'react';
 import Auth from './AuthContext';
 
 function Sign() {
