ff7a207f83dffb64f4bf2bc6808033fd520bec86
Yukta Agnihotri <yuktaagnihotri8585@gmail.com>
2025-08-20 20:32:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
         import svgr from 'vite-plugin-svgr';
  https://vite.dev/config/
-export default defineConfig({
+module.exports =  defineConfig({
   plugins: [react(),
       tailwindcss(),
       svgr(),
