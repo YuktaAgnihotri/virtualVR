c5cde4a7c382f521e82e6409be9c8f7affbfa89f
Yukta Agnihotri <yuktaagnihotri8585@gmail.com>
2025-08-25 02:02:19 +0530

    Update UserPg.jsx

--- src/components/UserPg.jsx
+++ src/components/UserPg.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import {Auth} from "/AuthContext"
 function UserPg() {
   return (
+     const {User,setUser }= createContext(Auth);
     <div>
- <h1 className='font-bold text-3xl'> Welcome </h1>
- <button> Log out </button>
+ <h1 className='font-bold text-3xl'> Welcome {User}</h1>
+ <button > Log out </button>
     </div>
   )
 }
